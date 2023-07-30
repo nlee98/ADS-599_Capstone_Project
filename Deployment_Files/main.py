0871e901a842b6982a0603f32bf060e5606bcfc9
nlee98 <lee.nicholas98@gmail.com>
2023-07-30 12:09:29 -0700

    Pipelines Saved and Loaded

--- Deployment_Files/main.py
+++ Deployment_Files/main.py
@@ -17,6 +17,10 @@ from sklearn.ensemble import GradientBoostingRegressor # Version 1.3.0
 house_model = pickle.load(open("Code Library/Models/gb_house_tuned.sav", "rb"))
 room_model = pickle.load(open("Code Library/Models/gb_room_tuned.sav", "rb"))
 
-st.header("Fish Weight Prediction App")
-# st.text_input("Enter your Name: ", key="name")
+# Load fitted pipelines
+house_pipeline = pickle.load(open("Deployment_Files/house_pipeline.sav", "rb"))
+room_pipeline = pickle.load(open("Deployment_Files/room_pipeline.sav", "rb"))
+
+st.header("San Diego Airbnb Price Estimator")
+st.text_input("Enter your Name: ", key="name")
 # data = pd.read_csv("fish.csv")
