8b352644611eeb890586309acd0fc25ec9903e90
nlee98 <lee.nicholas98@gmail.com>
2023-07-31 17:34:46 -0700

    Updated

--- Deployment_Files/main.py
+++ Deployment_Files/main.py
@@ -13,7 +13,8 @@ st.markdown("""
     Welcome to our application for predicting Airbnb prices in San Diego, California. 
 """)
 
-st.write("""\n\tThis application has two mode: URL Mode and Manual Mode.
-         In URL Mode, you can simply copy and paste the URL of an Airbnb listing of interest, enter the listed price,
-         and click _Price Prediction_ to get a price estimate
+st.write("""This application has two mode: URL Mode and Manual Mode.
+         \n\tIn URL Mode, you can simply copy and paste the URL of an Airbnb listing of interest, enter the listed price,
+         and click _Price Prediction_ to get a price estimate.
+         \n\tIn Manual Mode, you can manually enter some data regarding the property and the lister to get a price estimate.
          """)
